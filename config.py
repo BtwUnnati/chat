4ba32d873f113030445972ce180cf83823a39286
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-09-01 13:00:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 BOT_TOKEN = "8339967007:AAF8V_31TdXnEfwrRqXOvlyenGrLvC4usDU"
 SUPPORT_LINK = "https://t.me/chuckymusic_support"
 UPDATES_LINK = "https://t.me/teakadai_update"
-START_PHOTO_URL = "https://ar-hosting.pages.dev/1756639002914.jpg"  # Apne hosted ya imgur photo ka link yahan dalo
+START_PHOTO_URL = "https://cf-media.geiccobs.com/banner.jpg"  # Apne hosted ya imgur photo ka link yahan dalo
