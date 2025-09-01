4a96f03b8f0e31c401be22538c7b8f206e0ce6a9
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-09-01 12:33:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+SUPPORT_LINK = "https://t.me/ChatFightUpdates"
+UPDATES_LINK = "https://t.me/ChatFightUpdates"
+START_PHOTO_URL = "https://i.imgur.com/xo6xQXK.png"  # Apne hosted ya imgur photo ka link yahan dalo
