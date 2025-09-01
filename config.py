383a39dfd1e7844cdf88932ae51cc7efd3009425
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-09-01 12:37:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-SUPPORT_LINK = "https://t.me/ChatFightUpdates"
-UPDATES_LINK = "https://t.me/ChatFightUpdates"
-START_PHOTO_URL = "https://i.imgur.com/xo6xQXK.png"  # Apne hosted ya imgur photo ka link yahan dalo
+BOT_TOKEN = "8339967007:AAF8V_31TdXnEfwrRqXOvlyenGrLvC4usDU"
+SUPPORT_LINK = "https://t.me/chuckymusic_support"
+UPDATES_LINK = "https://t.me/teakadai_update"
+START_PHOTO_URL = "https://ar-hosting.pages.dev/1756639002914.jpg"  # Apne hosted ya imgur photo ka link yahan dalo
