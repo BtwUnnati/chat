d7f7831bc7cf0b06e4bd975812d8674dc594d077
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <DevUnnati86@gmail.com>
2025-09-02 19:26:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ BOT_TOKEN = "8339967007:AAFk6k-gsD1y7L8y_1Fj1CLGTGuGiAwmC9I"
 SUPPORT_LINK = "https://t.me/chuckymusic_support"
 UPDATES_LINK = "https://t.me/teakadai_update"
 START_PHOTO_URL = "https://cf-media.geiccobs.com/banner.jpg"  # Apne hosted ya imgur photo ka link yahan dalo
+OWNER_LINK = "https://t.me/teakadai_update"
